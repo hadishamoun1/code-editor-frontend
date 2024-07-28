f9b8c5a7dc1367d3c44d43a7eddbd145910db0aa
hadishamoun1 <shamounhadi2@gmail.com>
2024-07-28 14:35:51 +0300

    Added code editor componnt

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import CodeEditor from "./components/CodeEditor";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<CodeEditor />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
